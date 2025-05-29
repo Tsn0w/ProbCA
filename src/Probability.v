d22937fd153f9268fac706debd6190544e0e8409
Tsn0w <xsamarax00@gmail.com>
2025-05-29 23:19:10 +0300

    Update Probability.prob and multi_dist to be predicates

--- src/Probability.v
+++ src/Probability.v
@@ -11,66 +11,64 @@ Set Implicit Arguments.
 Unset Strict Implicit.
 Unset Printing Implicit Defensive.
 
-Declare Scope prob_scope.
-
 Module Probability.
-  Definition prob := { p : R | 0 <= p <= 1 }.
-  Definition prob_val (p : prob) : R := proj1_sig p.
+  Definition prob (p: R) := 0 <= p <= 1.
+  Check prob.
 
-  (* const probability values *)
-  Definition one : prob := exist _ R1 (conj Rle_0_1 (Rle_refl 1)).
-  Definition zero : prob := exist _ R0 (conj (Rle_refl 0) Rle_0_1).
-
-  (* probability operators *)
-  Definition mult (p1 p2 : prob) : prob.
+  Example prob_zero: prob 0.
+    Proof. split. lra. lra. Qed.
+  Example prob_one: prob 1.
+    Proof. split. lra. lra. Qed.
+  Lemma prob_mult: forall x y, prob x -> prob y -> prob (x * y).
   Proof.
-    destruct p1 as [x1 [H1x H1x']].
-    destruct p2 as [x2 [H2x H2x']].
-    exists (x1 * x2).
-    split.
-    - apply Rmult_le_pos; assumption.
-    - assert (H : 1 * 1 = 1). apply Rmult_1_r. rewrite <- H.
-      apply (Rmult_le_compat x1 1 x2 1); try assumption.
-  Defined.
-  Notation "p1 * p2" := (mult p1 p2) : prob_scope.
+    intros x y [Hx0 Hx1] [Hy0 Hy1]. split.
+    - apply Rmult_le_pos. apply Hx0. apply Hy0.
+    - replace 1 with (1 * 1) by lra. apply (Rmult_le_compat x 1 y 1).
+      apply Hx0. apply Hy0. apply Hx1. apply Hy1.
+  Qed.
 
-  Definition ge (p1 p2 : prob) : Prop := (proj1_sig p1) >= (proj1_sig p2).
-  Notation "p1 >= p2" := (ge p1 p2) : prob_scope.
+  Definition MDElem (T: Type) : Type := (T * R)%type.
+  Definition valid_MD_prob (p: R) := prob p /\ p > 0.
+  Definition valid_MDElem {T: Type} (e: MDElem T) :=  valid_MD_prob (snd e).
+  Definition total_prob (T: Type) (dist : list (MDElem T)) : R :=
+  fold_right (fun e acc => snd e + acc) 0 dist.
 
-  Definition gt (p1 p2: prob) :Prop := (proj1_sig p1) > (proj1_sig p2).
-  Notation "p1 > p2" := (gt p1 p2) : prob_scope.
+  Definition mult_dist (T: Type) (dist: list (MDElem T)) : Prop :=
+    total_prob dist <= 1 /\ Forall valid_MDElem dist.
 
-  Definition eq (p1 p2: prob) :Prop := (proj1_sig p1) = (proj1_sig p2).
-  Notation "p1 = p2" := (eq p1 p2) : prob_scope.
-
-  Delimit Scope prob_scope with prob.
-  Bind Scope prob_scope with prob.
-  Global Open Scope prob_scope.
-
-  Record MultiDist (T: Type) := {
-    support : list (T * prob);
-    valid_distribution : fold_right Rplus 0%R
-      (map (fun x => prob_val (snd x)) support) <= 1%R;
-  }.
-
-   Lemma support_prob_nonneg{T} (lst : list (T * prob)) :
-      0 <= fold_right Rplus 0 (map (fun x => prob_val (snd x)) lst).
-    Proof.
-      induction lst as [| [t p] tl IH].
-      - simpl; lra.
-      - simpl.
-        destruct p as [pval [Hp0 Hp1]].
-        apply Rplus_le_le_0_compat; assumption.
-    Qed.
-
-      Definition termination_probability {T: Type} (md: MultiDist T) : prob.
+  Definition flip_list := [(1, 0.5); (0, 0.5)].
+  Example flip_multidist: mult_dist flip_list.
   Proof.
-    destruct md as [lst Hvalid].
-    set (r := fold_right Rplus 0 (map (fun x => prob_val (snd x)) lst)).
-    assert (Hnonneg: 0 <= r) by (apply support_prob_nonneg).
-    exists r; split; [assumption | assumption].
-  Defined.
+     unfold mult_dist. split.
+     - simpl. lra.
+     - unfold valid_MDElem. unfold valid_MD_prob. unfold prob.
+       constructor.
+       + simpl. lra.
+       + constructor. simpl. lra. constructor.
+  Qed.
 
-  (* TODO: Add proof or definition? *)
-  Parameter prob_of : forall {T : Type}, MultiDist T -> T -> prob.
+  Definition multi_mult_dist {T U V: Type} (f : T -> U -> V) (dist1 : list (MDElem T)) (dist2 : list (MDElem U))
+      : list (MDElem V) :=
+    flat_map (fun e1: MDElem T =>
+                let (x1, p1) := e1 in
+                  map (fun e2 : MDElem U =>
+                    let (x2, p2) := e2 in (f x1 x2, p1 * p2)
+                  ) dist2
+              ) dist1.
+
+  Example flip_md_mult_flip_md_is_md: mult_dist (multi_mult_dist (fun x y => x + y) flip_list flip_list).
+  Proof.
+    unfold mult_dist. split.
+    - simpl. lra.
+    - simpl. unfold valid_MDElem. unfold valid_MD_prob. unfold prob. constructor.
+      + simpl. lra.
+      + constructor.
+        -- simpl. lra.
+        -- constructor.
+           ++ simpl. lra.
+           ++ constructor.
+              * simpl. lra.
+              * constructor.
+  Qed.
+
 End Probability.

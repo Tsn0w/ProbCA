37ece02cde87e513c6ec3db7e62e587733e4d1d0
Tsn0w <xsamarax00@gmail.com>
2025-05-31 11:29:25 +0300

    Probability: Organize proof and lemmas

--- src/ProbCA.v
+++ src/ProbCA.v
@@ -1,72 +1,84 @@
 Require Import ProbCA.Probability.
 Require Import ProbCA.Expressions.
+From Coq Require Import Reals.
+From Coq Require Import Lra.
 
 From Coq Require Import List.
 Import ListNotations.
 
-
-Open Scope prob_scope.
+Open Scope R_scope.
 
 Definition SET : Type := Type.
 Inductive Empty : SET :=.
-
 
 Module Type ProbabilityApplicativeStructure.
   (* set of Codes *)
   Parameter Code : SET.
 
-  (* The Application - on 2 codes returns multi-distribution of codes *)
-  Parameter App: Code -> Code -> Probability.MultiDist Code.
+  (*
+   * The Application - on 2 codes returns multi-distribution of codes
+   * applicaiton is valid is the result is indeed a multi-distribution
+   *)
+  Parameter App: Code -> Code -> list (Probability.md_elem Code).
+  (* Validate App *)
+  Definition VApp (cf ca: Code) : Prop := Probability.mult_dist (App cf ca).
 
-  (* Reduction relation, "infered" from the application *)
-  Parameter Red : Code -> Code -> Probability.prob -> Code -> Prop.
+  (* Paramter to compare codes, probably temporary till understand how to do this better! *)
+  Parameter eq_dec : forall (c c' : Code), {c = c'} + {c <> c'}.
+
+  (* Reduction relation, "infered" from the application
+   * The > can not be => 0, because than we than can have termination without any outcomes
+   * But this might be ok, since if we use p = 0, then we obviously want chaos.
+   *)
+  Definition Red (cf ca: Code) (p: R) (cr: Code) : Prop :=
+    VApp cf ca /\ Probability.prob p /\ Probability.prob_of eq_dec cr (App cf ca) > p.
 
   (* Application relation, "infered" from the application also but only care if there are any results *)
+  (* consider change to take p also *)
   Parameter Term : Code -> Code -> Prop.
 
   (* Progress: if terminates, exists a result *)
-  Parameter progress : forall cf ca : Code, Term cf ca -> exists (p: Probability.prob) (cr : Code),
-      Red cf ca p cr /\ Probability.gt p Probability.zero.
+  Parameter progress : forall cf ca : Code, Term cf ca -> exists (p: R) (cr : Code),
+      Red cf ca p cr /\ p >  0.
 
   (* Exsistence: iff reduces, can be "found" in the the md *)
-  Parameter existence: forall (cf ca cr : Code) (p: Probability.prob),
-  Red cf ca p cr <-> Probability.gt (Probability.prob_of (App cf ca) cr) p.
+  Lemma existence: forall (cf ca cr : Code) (p: R),
+  Red cf ca p cr <-> VApp cf ca /\ Probability.prob p /\ Probability.prob_of eq_dec cr (App cf ca) > p.
+  Proof.
+    intros cf ca cr p. unfold Red. reflexivity.
+  Qed.
 
-  (* define later! *)
-  Parameter Filter : Probability.MultiDist Code -> (Code -> Prop) ->  Probability.MultiDist Code -> Prop.
-
-  Module ProbabilityApplicativeExpression.
-    Export ApplicativeExpression.
-
-    (* RedExpr tells whether expression reduce to code c with probability p *)
-    Inductive RedExpr : Expr Code -> Probability.prob -> Code -> Prop
-    := revar (c : Code) : RedExpr (evar 0 c) Probability.one c
-     | reapp (ef ea : Expr Code) (cf ca cr: Code) (p1 p2 p3: Probability.prob):
-          RedExpr ef p1 cf -> RedExpr ea p2 ca -> Red cf ca p3 cr
-          -> RedExpr (eapp ef ea) (Probability.mult (Probability.mult p1 p2) p3) cr.
-
-    (* TermExpr tells whether expression reduce to a code *)
-    Inductive TermExpr : Expr Code -> Prop
-    := tevar (c : Code) : TermExpr (evar 0 c)
-     | teapp (ef ea : Expr Code) : TermExpr ef ->
-        (exists (cf : Code) (p1: Probability.prob), RedExpr ef p1 cf -> TermExpr ea /\
-          (exists (ca : Code) (p2: Probability.prob), RedExpr ea p2 ca -> Term cf ca)) -> TermExpr (eapp ef ea).
-
-    (* TermRed tells wheter application of 2 "models" predicate with probability p *)
-    Definition TermRed (cf ca : Code) (p : Probability.prob) (Pr : Code -> Prop) : Prop :=
-      Term cf ca /\ exists md : Probability.MultiDist Code, Filter (App cf ca) Pr md /\ Probability.ge (Probability.termination_probability md) p.
-
-    (* TermRedExpr tells whether expression "models" predicate with probability p *)
-    Fixpoint TermRedExpr (e : Expr Code) (p : Probability.prob) (Pr : Code -> Prop) : Prop :=
-      match e with
-        evar _ c => Pr c
-      | eapp ef ea => exists p1: Probability.prob, TermRedExpr ef p1
-           (fun cf => exists p2: Probability.prob, TermRedExpr ea p2
-              (fun ca => exists p3, TermRed cf ca p3 Pr /\ Probability.eq (Probability.mult (Probability.mult p1 p2) p3) p))
-      end.
-  End ProbabilityApplicativeExpression.
-
+  Lemma reduction_imply_lower: forall (p1 p2: R) (cf ca cr: Code),
+    Probability.prob p2 -> p1 > p2 -> Red cf ca p1 cr -> Red cf ca p2 cr.
+  Proof.
+    intros p1 p2 cf ca cr Vp2 Hlt [Happ [Hp1 Hgt]].
+    split; [assumption|]. split.
+    - apply Vp2.
+    - lra.
+  Qed.
 End ProbabilityApplicativeStructure.
+
+Module ProbabilityApplicativeExpression.
+  Include ProbabilityApplicativeStructure.
+  Export ApplicativeExpression.
+
+  (* RedExpr tells whether expression reduce to code c with probability p *)
+  Inductive RedExpr : Expr Code -> R -> Code -> Prop
+    :=
+      (* Ideally its enough to write only 1, but do I need to enforce the nubmer to be a valid probability? *)
+      revar (c : Code): RedExpr (evar 0 c) 1 c
+    | reapp (ef ea : Expr Code) (cf ca cr: Code) (p: R):
+        RedExpr ef p cf -> RedExpr ea p ca -> Red cf ca p cr
+  -> RedExpr (eapp ef ea) (p * p * p) cr.
+
+  (* TermExpr tells whether expression reduce to a code *)
+  Inductive TermExpr : Expr Code -> Prop
+  := tevar (c : Code) : TermExpr (evar 0 c)
+    | teapp (ef ea : Expr Code) : TermExpr ef -> (exists (p: R), Probability.prob p ->
+      (exists (cf : Code), RedExpr ef p cf -> TermExpr ea /\
+        (exists (ca : Code), RedExpr ea p ca -> Term cf ca))) -> TermExpr (eapp ef ea).
+
+End ProbabilityApplicativeExpression.
 
 Module Type ProbabilityCombinatoryAlgebra.
   Include ProbabilityApplicativeStructure.
@@ -77,12 +89,12 @@ Module Type ProbabilityCombinatoryAlgebra.
 
   (* $ \forall e \in E_1. \forall c_a, c_r. \forall p. c_{\lambda^0.e} \cdot c_a \downarrow_p c_r  \Longleftrightarrow  e[c_a] \downarrow_p c_r $ *)
   Parameter red_encode_0 : forall e: ExprVar Code 1,
-    forall (ca cr: Code) (p: Probability.prob), Red (cencode 0 e) ca p cr <-> RedExpr (esubst ca e) p cr.
+    forall (ca cr: Code) (p: R), Probability.prob p -> Red (cencode 0 e) ca p cr <-> RedExpr (esubst ca e) p cr.
 
   (* $ \forall n. \forall e \in E_{n+2}. \forall c_a, c_r . c_{\lambda^{n+1}.e} \cdot c_a \downarrow_1 c_r \Longrightarrow c_r =  c_{\lambda^{n}.e[c_a]} $ *)
   Parameter red_encode_S : forall n: nat,
     forall e : ExprVar Code (S (S n)), forall ca cr : Code,
-    Red (cencode (S n) e) ca Probability.one cr  -> cr = (cencode n (esubst ca e)).
+    Red (cencode (S n) e) ca 1 cr  -> cr = (cencode n (esubst ca e)).
 
   (* $ \forall e \in E_1. \forall c_a. \forall p e[c_a] \downarrow \Longleftrightarrow c_{\lambda^0.e} \cdot c_a \downarrow $ *)
   Parameter term_encode_0 : forall e: ExprVar Code 1,
@@ -92,14 +104,3 @@ Module Type ProbabilityCombinatoryAlgebra.
   Parameter term_encode_S : forall n: nat,
     forall e : ExprVar Code (S (S n)), forall ca: Code, Term (cencode (S n) e) ca.
 End ProbabilityCombinatoryAlgebra.
-
-Module ProbabilityCombinatoryAlgebraTools (ProbCA : ProbabilityCombinatoryAlgebra).
-  Import ProbCA.
-  Import ProbabilityApplicativeExpression.
-
-  (* if forall codes Pr implies Pr', then if TermRed cf ca p Pr holds then TermRed cf ca p Pr' holds *)
-  Lemma termred_mono (cf ca : Code) (p: Probability.prob) (Pr Pr' : Code -> Prop) : (forall cr : Code, Pr cr -> Pr' cr) -> TermRed cf ca p Pr -> TermRed cf ca p Pr'.
-  Proof.
-  Admitted.
-
-End ProbabilityCombinatoryAlgebraTools.
--- src/Probability.v
+++ src/Probability.v
@@ -1,7 +1,7 @@
-
 From Coq Require Import Reals.
 From Coq Require Import List.
 From Coq Require Import Lra.
+Require Import Coq.Lists.ListSet.
 
 Import ListNotations.
 
@@ -12,8 +12,10 @@ Unset Strict Implicit.
 Unset Printing Implicit Defensive.
 
 Module Probability.
+  (***********************************************************************************************)
+  (*                                 Probability definition                                      *)
+  (***********************************************************************************************)
   Definition prob (p: R) := 0 <= p <= 1.
-  Check prob.
 
   Example prob_zero: prob 0.
     Proof. split. lra. lra. Qed.
@@ -27,40 +29,127 @@ Module Probability.
       apply Hx0. apply Hy0. apply Hx1. apply Hy1.
   Qed.
 
-  Definition MDElem (T: Type) : Type := (T * R)%type.
-  Definition valid_MD_prob (p: R) := prob p /\ p > 0.
-  Definition valid_MDElem {T: Type} (e: MDElem T) :=  valid_MD_prob (snd e).
-  Definition total_prob (T: Type) (dist : list (MDElem T)) : R :=
-  fold_right (fun e acc => snd e + acc) 0 dist.
 
-  Definition mult_dist (T: Type) (dist: list (MDElem T)) : Prop :=
-    total_prob dist <= 1 /\ Forall valid_MDElem dist.
+  (***********************************************************************************************)
+  (*                                 multi-distribution definition                               *)
+  (***********************************************************************************************)
+  Definition md_elem (T: Type) : Type := (T * R)%type.
+  Definition valid_md_prob (p: R) := prob p /\ p > 0.
+  Definition valid_md_elem {T: Type} (e: md_elem T) :=  valid_md_prob (snd e).
+  Fixpoint total_prob (T: Type) (l: list(md_elem T)) : R :=
+    match l with
+      | [] => 0
+      | x :: l' => (snd x) + (total_prob l')
+    end.
 
-  Definition flip_list := [(1, 0.5); (0, 0.5)].
+  Definition mult_dist (T: Type) (dist: list (md_elem T)) : Prop :=
+    total_prob dist <= 1 /\ Forall valid_md_elem dist.
+
+  Example empty_md: forall {T: Type}, mult_dist ([] : list (md_elem T)).
+  Proof.
+    intro T. simpl. unfold mult_dist. simpl. split.
+    - lra.
+    - constructor.
+  Qed.
+
+  Definition flip_list: list (md_elem nat):= [(1%nat, 0.5); (0%nat, 0.5)].
   Example flip_multidist: mult_dist flip_list.
   Proof.
      unfold mult_dist. split.
      - simpl. lra.
-     - unfold valid_MDElem. unfold valid_MD_prob. unfold prob.
+     - unfold valid_md_elem. unfold valid_md_prob. unfold prob.
        constructor.
        + simpl. lra.
        + constructor. simpl. lra. constructor.
   Qed.
 
-  Definition multi_mult_dist {T U V: Type} (f : T -> U -> V) (dist1 : list (MDElem T)) (dist2 : list (MDElem U))
-      : list (MDElem V) :=
-    flat_map (fun e1: MDElem T =>
-                let (x1, p1) := e1 in
-                  map (fun e2 : MDElem U =>
-                    let (x2, p2) := e2 in (f x1 x2, p1 * p2)
-                  ) dist2
-              ) dist1.
+  Lemma total_prob_geq_zero {T: Type} : forall (l: list (md_elem T)), mult_dist l -> 0 <= total_prob l.
+  Proof.
+  intros l Hdist.
+  induction l as [| [x p] l' IH].
+  - simpl. lra.
+  - simpl in *. destruct Hdist as [Htotal Hforalls].
+    inversion Hforalls as [| [x' p'] l'' Hvalid_elem Hrest_eq]; subst.
+    unfold valid_md_elem, valid_md_prob, prob in Hvalid_elem.
+    destruct Hvalid_elem as [[Hp0 _] Hp_pos]. apply Rplus_le_le_0_compat.
+    simpl in Hp0.
+    + apply Hp0.
+    + apply IH. split.
+      * simpl in Htotal. simpl in Hp0. lra.
+      * apply Hrest_eq.
+  Qed.
 
-  Example flip_md_mult_flip_md_is_md: mult_dist (multi_mult_dist (fun x y => x + y) flip_list flip_list).
+
+  (***********************************************************************************************)
+  (*                                    Probability of Element                                   *)
+  (***********************************************************************************************)
+  Fixpoint prob_of {T: Type} (eq_dec : forall a b : T, {a = b} + {a <> b}) (t: T) (l: list (T * R)) : R :=
+  match l with
+    | [] => 0
+    | (t', p) :: xs => if eq_dec t' t then p + prob_of eq_dec t xs else prob_of eq_dec t xs
+  end.
+
+  Example prob_of_1_at_flip_is_half: prob_of Nat.eq_dec 1%nat (flip_list) = 0.5.
+  Proof.
+    unfold prob_of. simpl. rewrite Rplus_0_r. reflexivity.
+  Qed.
+
+  (***********************************************************************************************)
+  (*                                 Multiplication by scalar                                    *)
+  (***********************************************************************************************)
+  Fixpoint mult_md_by_scalar {T: Type} (p: R) (l: list (md_elem T)) : list (md_elem T) :=
+    match l with
+      | [] => []
+      | (x1, p1) :: xs1 => (x1, p1 * p)::(mult_md_by_scalar p xs1)
+    end.
+
+  Lemma total_prob_scalar {T} (p: R) (l: list (md_elem T)) :
+  total_prob (mult_md_by_scalar p l) = p * total_prob l.
+Proof.
+  induction l as [| [x q] l' IH]; simpl.
+  - lra.
+  - rewrite IH. lra.
+Qed.
+
+  Lemma scalar_multi_md {T: Type} : forall (p: R) (l: list (md_elem T)),
+    prob p /\ p > 0 -> mult_dist l ->  mult_dist (mult_md_by_scalar p l).
+  Proof.
+    intros p l [[p0 p1] p0'] [Htotal Hvalid].
+  split.
+  - rewrite total_prob_scalar.
+    replace 1 with (1 * 1) by lra.
+    apply (Rmult_le_compat p 1 (total_prob l) 1); try lra.
+    apply total_prob_geq_zero. split; assumption.
+  - induction Hvalid as [| [x q] l' Hq_valid Hrest IH].
+    + simpl. constructor.
+    + simpl. constructor.
+      * unfold valid_md_elem, valid_md_prob, prob in *.
+        destruct Hq_valid as [[Hq0 Hq1] Hq_pos].
+        split.
+        -- simpl. simpl in Hq0, Hq1. split.
+           ++ apply Rmult_le_pos; lra.
+           ++ replace 1 with (1 * 1) by lra. apply Rmult_le_compat; lra.
+        -- simpl. simpl in Hq_pos. apply Rmult_lt_0_compat. lra. apply p0'.
+      * simpl in Htotal. unfold valid_md_elem in Hq_valid. simpl in Hq_valid.
+        destruct Hq_valid as [q0 q1]. apply IH. lra.
+Qed.
+
+
+  (***********************************************************************************************)
+  (*                                 Multiplication of mdists                                    *)
+  (***********************************************************************************************)
+  Fixpoint multi_list {T U V: Type} (f : T -> U -> V) (list1 : list (md_elem T)) (list2 : list (md_elem U))
+      : list (md_elem V) :=
+      match list1 with
+        | [] => []
+        | (x, p) :: t => (map (fun '(x', p') => (f x x', p * p')) list2) ++ multi_list f t list2
+      end.
+
+  Example flip_list_mult_flip_list_is_md: mult_dist (multi_list (fun x y => (x + y)%nat) flip_list flip_list).
   Proof.
     unfold mult_dist. split.
     - simpl. lra.
-    - simpl. unfold valid_MDElem. unfold valid_MD_prob. unfold prob. constructor.
+    - simpl. unfold valid_md_elem. unfold valid_md_prob. unfold prob. constructor.
       + simpl. lra.
       + constructor.
         -- simpl. lra.
@@ -70,5 +159,183 @@ Module Probability.
               * simpl. lra.
               * constructor.
   Qed.
+
+  Lemma multi_list_app_split {T U V: Type}: forall (f: T -> U -> V) (x: md_elem T) (l1 : list (md_elem T)) (l2 : list (md_elem U)),
+    multi_list f (x :: l1) l2 = (multi_list f [x] l2) ++ (multi_list f l1 l2).
+  Proof.
+    intros f x l1 l2.  destruct x as [xv xp]. simpl. rewrite app_nil_r. reflexivity.
+  Qed.
+
+  Lemma total_prob_split {T: Type}: forall (l1 l2: list (md_elem T)),
+    total_prob l1 + total_prob l2 = total_prob (l1 ++ l2).
+  Proof.
+    intros l1 l2. induction l1 as [| [x p] l1' IHl1].
+    - simpl. lra.
+    - simpl. rewrite <- IHl1. lra.
+  Qed.
+
+Lemma total_prob_multi_list_nil : forall (T U V : Type) (f : T -> U -> V) (x : T) (p : R),
+  total_prob (multi_list f [(x, p)] []) = p * (total_prob ([] : list (md_elem V))).
+Proof.
+  intros T U V f x p. simpl. lra.
+Qed.
+
+Lemma total_prob_mult_elem {T U V: Type}: forall  (f: T -> U -> V) (x: md_elem T) (l: list (md_elem U)),
+    total_prob [x] * total_prob l = total_prob (multi_list f [x] l).
+  Proof.
+    intros f [xv xp] l. induction l as [| [x' p'] l' IHl'].
+    - simpl. lra.
+    - simpl. simpl in IHl'. rewrite <- IHl'. lra.
+  Qed.
+
+
+  Lemma total_prob_mult {T U V: Type}: forall  (f: T -> U -> V) (l1: list (md_elem T)) (l2: list (md_elem U)),
+    total_prob l1 * total_prob l2 = total_prob (multi_list f l1 l2).
+  Proof.
+    intros f l1 l2. induction l1 as [| [x p] l1' IHl1'].
+    - simpl. lra.
+    - rewrite multi_list_app_split. rewrite <- total_prob_split. rewrite <- IHl1'.
+      change (total_prob ((x, p) :: l1')) with (total_prob ([(x, p)] ++ l1')).
+      assert (total_prob ([(x, p)] ++ l1') = total_prob [(x, p)] + total_prob l1').
+        { simpl. lra. }
+        rewrite H. rewrite <- total_prob_mult_elem. simpl. lra.
+  Qed.
+
+  Lemma valid_list_total_prob_geq_zero {T : Type} :
+  forall (l : list (md_elem T)),
+    Forall valid_md_elem l ->
+    0 <= total_prob l.
+  Proof.
+    intros l Hvalid. induction Hvalid.
+  - simpl. lra.
+  - simpl. destruct x as [xv pval].
+    assert (pval_ge_0: 0 <= pval).
+      { unfold valid_md_elem, valid_md_prob in H. simpl in H. apply H. }
+      simpl. lra.
+  Qed.
+
+  Lemma head_tail_md_is_md {T: Type}: forall (x: md_elem T) (l: list (md_elem T)),
+    mult_dist (x :: l) -> mult_dist [x] /\ mult_dist l.
+  Proof.
+    intros [x0 p0] l Hmd.
+    assert (pos_md: 0 <= total_prob ((x0, p0) :: l)).
+      { apply total_prob_geq_zero.  apply Hmd. }
+      destruct Hmd as [Ht Hv]. simpl in Ht.
+      simpl in pos_md.
+      inversion Hv as [|x l' Hfx Hfl Heq]; subst.
+      unfold valid_md_elem, valid_md_prob in Hfx. simpl in Hfx.
+      destruct Hfx as [Hpp0 Hg0p0].
+      assert (total_prob_l_0: 0<= total_prob l).
+        { apply valid_list_total_prob_geq_zero. apply Hfl. }
+      assert (total_prob_l_1: total_prob l <= 1).
+        { lra. }
+      split.
+      - split.
+        + simpl. lra.
+        + constructor.
+          * unfold valid_md_elem, valid_md_prob. simpl. apply (conj Hpp0 Hg0p0).
+          * constructor.
+      - apply (conj total_prob_l_1 Hfl).
+  Qed.
+
+  Lemma multi_list_split_r {T U V: Type}:
+    forall (f: T -> U -> V) (l1 : list (md_elem T)) (x: md_elem T) (l2 : list (md_elem U)),
+    multi_list f (x::l1) l2 = multi_list f [x] l2 ++ multi_list f l1 l2.
+  Proof.
+    intros f l1 x l2.  destruct x as [tx px].
+     simpl. rewrite app_nil_r. reflexivity.
+  Qed.
+
+  Lemma Forall_map :
+    forall (A B : Type) (f : A -> B) (P : B -> Prop) (l : list A),
+      Forall P (map f l) <-> Forall (fun x => P (f x)) l.
+  Proof.
+    intros A B f P l. split.
+    - induction l; simpl.
+      + constructor.
+      + intros H. inversion H. constructor; auto.
+    - induction l; simpl.
+      + constructor.
+      + intros H. inversion H. constructor; auto.
+  Qed.
+
+  Lemma valid_md_elem_map :
+    forall (T U V : Type) (f : T -> U -> V) (x1 : T) (p1 : R) (x' : U) (p' : R),
+      valid_md_elem (x1, p1) ->
+      valid_md_elem (x', p') ->
+      valid_md_elem (f x1 x', p1 * p').
+  Proof.
+    intros T U V f x1 p1 x' p' Hvalid1 Hvalid2.
+    unfold valid_md_elem in *.
+    destruct Hvalid1 as [Hle1 Hgt1].
+    destruct Hvalid2 as [Hle2 Hgt2].
+    simpl in *. unfold prob in *. destruct Hle1 as [Hle01 Hlg11].
+    destruct Hle2 as [Hle02 Hlg12]. split.
+    - unfold prob. split.
+      + apply Rmult_le_pos; lra.
+      + replace 1 with (1 * 1) by lra. apply Rmult_le_compat; lra.
+    - apply Rmult_lt_0_compat; lra.
+  Qed.
+
+  Lemma multi_list_of_valid_elems_is_valid_elems {T U V: Type}:
+      forall (f: T -> U -> V) (l1 : list (md_elem T)) (l2 : list (md_elem U)),
+        Forall valid_md_elem l1 -> Forall valid_md_elem l2 ->
+          Forall valid_md_elem (multi_list f l1 l2).
+  Proof.
+    intros f l1 l2 Hvalid1 Hvalid2.
+    induction Hvalid1 as [| [x1 p1] l1' Hx1_valid Hl1' IH].
+    - simpl. constructor.
+    - rewrite multi_list_split_r. apply Forall_app. split.
+      + simpl. rewrite app_nil_r.
+        apply Forall_map. apply Forall_forall. intros [x' p'] Hin.
+        apply valid_md_elem_map.
+        * apply Hx1_valid.
+        * apply Forall_forall with (x := (x', p')) in Hvalid2; auto.
+      + apply IH.
+  Qed.
+
+
+  Lemma multi_md_md_is_md {T U V: Type} : forall (f: T -> U -> V) (l1 : list (md_elem T)) (l2 : list (md_elem U)),
+    mult_dist l1 -> mult_dist l2 -> mult_dist (multi_list f l1 l2).
+  Proof.
+    intros f l1 l2 mdl1 mdl2.
+    + induction l1 as [| [x p] l1' C].
+      - simpl. apply empty_md.
+      - destruct (@head_tail_md_is_md T (x, p) l1' mdl1) as [mdx mdl1'].
+        assert (total_l1_pos : 0 <= total_prob l1').
+          { apply total_prob_geq_zero. apply mdl1'. }
+      destruct mdl1 as [l1total l1elems].
+        simpl in l1total. rewrite multi_list_app_split. unfold mult_dist. split.
+        * rewrite <- total_prob_split. rewrite <- total_prob_mult.
+          assert (total_prob (multi_list f l1' l2) = total_prob l1' * total_prob l2).
+            { rewrite <- total_prob_mult. reflexivity. }
+          rewrite H. simpl.
+          assert ((p + 0) * total_prob l2 + total_prob l1' * total_prob l2 = (p + total_prob l1') * total_prob l2).
+            { lra. }
+            rewrite H0. replace 1 with (1 *1) by lra. apply Rmult_le_compat.
+            --  inversion l1elems as [| ?? Hhead Htail].
+                apply Rplus_le_le_0_compat; try lra.
+               ++ unfold valid_md_elem,valid_md_prob in Hhead. simpl in Hhead. lra.
+            -- apply total_prob_geq_zero. apply mdl2.
+            -- apply l1total.
+            -- apply mdl2.
+        * apply Forall_app. split.
+            -- apply multi_list_of_valid_elems_is_valid_elems.
+               ++ constructor. apply Forall_inv in l1elems. apply l1elems.
+                  constructor.
+               ++ apply mdl2.
+            -- apply C. apply mdl1'.
+  Qed.
+
+
+
+  (***********************************************************************************************)
+  (*                                     Filter of mdist                                         *)
+  (***********************************************************************************************)
+  Fixpoint filter_md {T : Type} (Pr : T -> bool) (l : list (md_elem T)) : list (md_elem T) :=
+  match l with
+  | [] => []
+  | x :: xs => if Pr (fst x) then x :: filter_md Pr xs else filter_md Pr xs
+  end.
 
 End Probability.
